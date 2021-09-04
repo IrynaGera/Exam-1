71fdf1c54b7842f6d730d9ea640b2b06becb7bca
IrynaGera <iragerasimovich789@gmail.com>
2021-09-04 09:39:21 +0300

    exam-1

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.js-burger-btn').on('click', function () {
+        if ($('.js-header').hasClass('active')) {
+            $('.js-header').removeClass('active');
+        } else {
+            $('.js-header').addClass('active');
+        }
+    })
+});
+
